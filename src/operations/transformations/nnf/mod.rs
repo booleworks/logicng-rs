1bb6196b63d8dfc8e13783bde6189efb01286ede
Matthias Jugan <matthias.jugan@booleworks.com>
2023-12-01 11:32:41 +0100

    Improve nnf caching

--- src/operations/transformations/nnf/mod.rs
+++ src/operations/transformations/nnf/mod.rs
@@ -23,6 +23,13 @@ pub fn nnf(formula: EncodedFormula, f: &FormulaFactory) -> EncodedFormula {
 
 fn nnf_rec(formula: EncodedFormula, f: &FormulaFactory, polarity: bool) -> EncodedFormula {
     if polarity {
+        if f.caches.is_nnf.get(formula) == Some(true)
+            || f.caches.is_cnf.get(formula).is_some()
+            || f.caches.is_dnf.get(formula) == Some(true)
+        {
+            return formula;
+        }
+
         if let Some(nnf) = f.caches.nnf.get(formula) {
             return nnf;
         }
