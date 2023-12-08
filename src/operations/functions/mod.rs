2dcfae7d374f58f9f3200a84171b8da2a5b64e91
Matthias Jugan <matthias.jugan@booleworks.com>
2023-12-08 13:01:24 +0100

    Add model counting functions

--- src/operations/functions/mod.rs
+++ src/operations/functions/mod.rs
@@ -1,11 +1,13 @@
 mod formula_depth_function;
 mod literals;
+mod model_count;
 mod operands;
 mod sub_node_function;
 mod variable_profile_function;
 
 pub use formula_depth_function::*;
 pub use literals::*;
+pub use model_count::*;
 pub use operands::*;
 pub use sub_node_function::*;
 pub use variable_profile_function::*;
