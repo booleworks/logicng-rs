19912a9aeae2faef9e363a18dcdffec20c460dcf
Matthias Jugan <matthias.jugan@booleworks.com>
2023-12-13 14:56:40 +0100

    comments for sharpsat module

--- src/solver/mod.rs
+++ src/solver/mod.rs
@@ -367,6 +367,32 @@ pub mod maxsat {
     pub use super::maxsat_solver::*;
 }
 
+/// Provides are solver of solver for the #SAT problem. It uses an external C++
+/// solver based in the [implementation from Marc
+/// Thurley](https://github.com/marcthurley/sharpSAT).
+///
+/// In order to use this module you need to enable the `sharp_sat` feature and
+/// ensure the library compiles properly on your device (this is usually the
+/// hard part).  
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// # use logicng::formulas::{FormulaFactory, ToFormula};
+/// # use num_bigint::BigUint;
+/// use logicng::solver::sharpsat::SharpSatSolver;
+///
+/// let f = FormulaFactory::new();
+/// let mut solver = SharpSatSolver::new();
+///
+/// solver.add_cnf("(a | c) & (~a | b | ~d)".to_formula(&f), &f);
+/// solver.add_cnf("(~c | ~d)".to_formula(&f), &f);
+///
+/// let count = solver.solve();
+/// assert_eq!(count, BigUint::from(7_u32));
+/// ```
 #[cfg(feature = "sharp_sat")]
 pub mod sharpsat;
 
--- src/solver/sharpsat.rs
+++ src/solver/sharpsat.rs
@@ -6,6 +6,39 @@ use num_bigint::BigUint;
 
 use crate::formulas::{EncodedFormula, Formula, FormulaFactory, Literal, Variable};
 
+/// Wrapper for a [external #SAT
+/// implementation](https://github.com/booleworks/logicng-sharp-sat). Allows for
+/// adding clauses and formulas in CNF and counting the models of the
+/// conjunction of all added clauses.
+///
+/// Use [`count_models()`](crate::operations::functions::count_models) and
+/// [`count_models_conjunction()`](crate::operations::functions::count_models_conjunction)
+/// (or related functions) for counting models of arbitrary formulas.
+///
+/// The external solver is based on this [implementation from Marc
+/// Thurley](https://github.com/marcthurley/sharpSAT).
+///
+/// In order to use this solver you need to enable the `sharp_sat` feature and
+/// ensure the library compiles properly on your device.
+///
+/// # Examples
+///
+/// Basic usage:
+///
+/// ```
+/// # use logicng::formulas::{FormulaFactory, ToFormula};
+/// # use num_bigint::BigUint;
+/// use logicng::solver::sharpsat::SharpSatSolver;
+///
+/// let f = FormulaFactory::new();
+/// let mut solver = SharpSatSolver::new();
+///
+/// solver.add_cnf("(a | c) & (~a | b | ~d)".to_formula(&f), &f);
+/// solver.add_cnf("(~c | ~d)".to_formula(&f), &f);
+///
+/// let count = solver.solve();
+/// assert_eq!(count, BigUint::from(7_u32));
+/// ```
 pub struct SharpSatSolver {
     solver: *mut ffi::Solver,
     var_map_down: HashMap<Variable, i32>,
@@ -20,10 +53,36 @@ impl Default for SharpSatSolver {
 }
 
 impl SharpSatSolver {
+    /// Creates a new [`SharpSatSolver`] instance.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// use logicng::solver::sharpsat::SharpSatSolver;
+    ///
+    /// let mut solver = SharpSatSolver::new();
+    /// ```
     pub fn new() -> Self {
         Self { solver: unsafe { ffi::new_solver() }, var_map_down: HashMap::default(), var_map_up: Vec::default(), constant: None }
     }
 
+    /// Adds a clause to the solver.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use logicng::formulas::FormulaFactory;
+    /// # use logicng::solver::sharpsat::SharpSatSolver;
+    ///
+    /// let f = FormulaFactory::new();
+    /// let mut solver = SharpSatSolver::new();
+    ///
+    /// solver.add_clause(&[f.lit("a", true), f.lit("b", true), f.lit("c", false)]);
+    /// ```
     pub fn add_clause(&mut self, clause: &[Literal]) {
         if clause.is_empty() {
             self.constant = Some(false);
@@ -54,6 +113,22 @@ impl SharpSatSolver {
         }
     }
 
+    /// Add a formula in CNF to the solver. This function panics if the passed
+    /// formula is not in CNF.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use logicng::formulas::{FormulaFactory, ToFormula};
+    /// # use logicng::solver::sharpsat::SharpSatSolver;
+    ///
+    /// let f = FormulaFactory::new();
+    /// let mut solver = SharpSatSolver::new();
+    ///
+    /// solver.add_cnf("(a | b) & (c | a | d) & (a | d)".to_formula(&f), &f);
+    /// ```
     pub fn add_cnf(&mut self, cnf_formula: EncodedFormula, f: &FormulaFactory) {
         match cnf_formula.unpack(f) {
             Formula::Or(ops) => {
@@ -97,6 +172,27 @@ impl SharpSatSolver {
         }
     }
 
+    /// Counts the models of the formula on this solver.
+    ///
+    /// Note that while adding formulas some preprocessing might have discovered
+    /// that this formula is trivial. In this case, the result will be returned
+    /// directly and the external solver will not be called.
+    ///
+    /// # Examples
+    ///
+    /// Basic usage:
+    ///
+    /// ```
+    /// # use logicng::formulas::FormulaFactory;
+    /// # use logicng::solver::sharpsat::SharpSatSolver;
+    ///
+    /// let f = FormulaFactory::new();
+    /// let mut solver = SharpSatSolver::new();
+    ///
+    /// // add formulas to the solver ...
+    ///
+    /// let count = solver.solve();
+    /// ```
     pub fn solve(self) -> BigUint {
         match self.constant {
             Some(true) => BigUint::from(1_u64),
