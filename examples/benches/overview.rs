52ddde15de0b8bdf64f56f67cc2b0b916ea5a916
Matthias Jugan <matthias.jugan@booleworks.com>
2024-01-04 13:59:41 +0100

    bench_overview picks benchmarks based on active feature

--- examples/benches/overview.rs
+++ examples/benches/overview.rs
@@ -1,21 +1,29 @@
 use std::fs;
 
 use itertools::Itertools;
-use logicng::formulas::{EncodedFormula, FormulaFactory};
+use logicng::formulas::FormulaFactory;
 use logicng::io::read_formula;
-use logicng::knowledge_compilation::dnnf::{compile_dnnf, count};
 use logicng::operations::functions::{count_models, ModelCountAlgorithm};
-use logicng::operations::transformations::{pure_expansion, AdvancedFactorizationConfig, CnfAlgorithm, CnfEncoder};
-use logicng::solver::maxsat::{Algorithm, MaxSatSolver};
 use logicng::solver::minisat::MiniSat;
+
+#[cfg(feature = "open_wbo")]
+use logicng::solver::maxsat::{Algorithm, MaxSatSolver};
 
 fn main() {
     parse();
     parse();
     sat();
     model_counting();
-    model_counting_sharpsat();
-    maximize_maxsat();
+
+    #[cfg(feature = "sharp_sat")]
+    {
+        model_counting_sharpsat();
+    }
+
+    #[cfg(feature = "open_wbo")]
+    {
+        maximize_maxsat();
+    }
 }
 
 fn sat() {
@@ -58,16 +66,12 @@ fn model_counting() {
 
     let start = std::time::Instant::now();
     for formula in formulas {
-        let expanded = pure_expansion(formula, &f);
-        let mut cnf_encoder =
-            CnfEncoder::new(CnfAlgorithm::Advanced(AdvancedFactorizationConfig::default().fallback_algorithm(CnfAlgorithm::Tseitin)));
-        let cnf_formula = cnf_encoder.transform(expanded, &f);
-        let dnnf = compile_dnnf(cnf_formula, &f);
-        count(&dnnf, &f);
+        count_models(formula, ModelCountAlgorithm::Dnnf, &f);
     }
     println!("Rust Model Counting (Dnnf): {}s", start.elapsed().as_secs_f32());
 }
 
+#[cfg(feature = "sharp_sat")]
 fn model_counting_sharpsat() {
     let f = FormulaFactory::new();
     let formulas = fs::read_dir("./resources/formula_suite_1/")
@@ -82,6 +86,7 @@ fn model_counting_sharpsat() {
     println!("Rust Model Counting (SharpSAT): {}s", start.elapsed().as_secs_f32());
 }
 
+#[cfg(feature = "open_wbo")]
 fn maximize_maxsat() {
     let f = FormulaFactory::new();
     let formulas = fs::read_dir("./resources/formula_suite_1/")
@@ -95,7 +100,7 @@ fn maximize_maxsat() {
         let mut solver = MaxSatSolver::new(Algorithm::Oll).unwrap();
         solver.add_hard_formula(formula, &f).unwrap();
         for var in &*variables {
-            solver.add_soft_formula(1, EncodedFormula::from(var.pos_lit()), &f).unwrap();
+            solver.add_soft_formula(1, var.pos_lit().into(), &f).unwrap();
         }
         let _res = solver.solve().unwrap();
     }
