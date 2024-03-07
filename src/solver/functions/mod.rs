d250ee597832ab1c2fa80538d492a9a45c998771
Matthias Jugan <matthias.jugan@booleworks.com>
2024-03-07 15:09:52 +0100

    Move formula_on_solver function to minisat_solver.rs

--- src/solver/functions/mod.rs
+++ src/solver/functions/mod.rs
@@ -1,11 +1,9 @@
 mod backbone_function;
-mod formula_on_solver_function;
 mod model_enumeration;
 mod optimization_function;
 mod unsat_core_function;
 
 pub use backbone_function::*;
-pub use formula_on_solver_function::*;
 pub use model_enumeration::*;
 pub use optimization_function::*;
 pub use unsat_core_function::*;
--- src/solver/minisat_solver.rs
+++ src/solver/minisat_solver.rs
@@ -1,6 +1,8 @@
 #![allow(clippy::cast_possible_wrap, clippy::cast_sign_loss)]
 
 use std::collections::{BTreeSet, HashMap};
+use std::ops::Deref;
+use std::sync::Arc;
 
 use crate::cardinality_constraints::{CcEncoder, CcIncrementalData};
 use crate::collections::LNG_VEC_INIT_SIZE;
@@ -298,6 +300,39 @@ impl<B> MiniSat<B> {
     /// optimization handler aborted the computation, `None` will be returned.
     pub fn optimize(&mut self, f: &FormulaFactory, optimization_function: &OptimizationFunction) -> Option<Model> {
         optimization_function.optimize(self, f)
+    }
+
+    /// Returns the current formula on the solver.
+    ///
+    /// Note that this formula is usually syntactically different from the
+    /// formulas which were actually added to the solver, since the formulas are
+    /// added as CNF and may be simplified or even removed, depending on the
+    /// state of the solver. Furthermore, the solver might add learnt clauses or
+    /// propagate literals. This can be useful to debug or analyze a solver at a
+    /// given time.
+    pub fn formula_on_solver(&self, f: &FormulaFactory) -> Arc<[EncodedFormula]> {
+        let mut formulas = Vec::new();
+        for clause_ref in &self.underlying_solver.clauses {
+            let lits: Vec<Literal> = clause_ref
+                .borrow()
+                .deref()
+                .data
+                .iter()
+                .map(|&lit| Literal::new(self.underlying_solver.variable_for_idx(var(lit)).unwrap(), !sign(lit)))
+                .collect();
+            formulas.push(f.clause(&lits));
+        }
+        self.underlying_solver
+            .vars
+            .iter()
+            .enumerate()
+            .filter(|(_, var)| var.level == Some(0))
+            .map(|(i, var)| Literal::new(self.underlying_solver.variable_for_idx(MsVar(i)).unwrap(), var.assignment == True))
+            .for_each(|lit| formulas.push(lit.into()));
+        if !self.underlying_solver.ok {
+            formulas.push(f.falsum());
+        }
+        formulas.into()
     }
 
     fn add_all_original_variables(&mut self, formula: EncodedFormula, f: &FormulaFactory) {
